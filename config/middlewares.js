b9d94630d2f816f338f0d4da4d6f8e1debc87d2f
miszczil <sara@leanconvert.com>
2023-11-23 19:18:08 +0100

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,34 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://market-assets.strapi.io",
+            "https://tile.openstreetmap.org",
+            "https://a.tile.openstreetmap.org",
+            "https://b.tile.openstreetmap.org",
+            "https://c.tile.openstreetmap.org",
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
---
+++ config/plugins.js
@@ -0,0 +1,5 @@
+module.exports = ({ env }) => ({
+	"location-plugin": {
+		enabled:  true,
+	},
+});
